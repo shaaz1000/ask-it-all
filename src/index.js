04bc7eec2a465ae79ace12076ccaa54331b0032a
sofiyarao <appsmadeforu@gmail.com>
2024-05-05 18:27:19 +0530

    [UPDATE] fix - dependency tree

--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from "react";
 import "./index.css";
 import "./index.scss";
 import "./static/assets/fonts/poppins.css";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const theme = createTheme({
-  pallete: {
-      primary: "#eb5757",
-  },
-});
 
 root.render(
-  <MuiThemeProvider theme={theme}>
     <AppRoutes />
-  </MuiThemeProvider>
 );
